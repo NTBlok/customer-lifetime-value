d53df368eeab49d775c4cfdff0d7296aa7e21904
NTBlok <ntedeschiblok@gmail.com>
2017-02-24 07:27:50 -0800

    Generated fake data for input

--- src/data/fake_events.py
+++ src/data/fake_events.py
@@ -1,14 +1,19 @@
 from faker import Factory
 import datetime
+from datetime import datetime
+from datetime import timedelta
 import json
 import random
+from random import randint
 
 from CameraProvider import CameraProvider
 
 
 
-def fake_event_time():
-    event_time = datetime.datetime.utcnow().isoformat()
+def fake_event_time(days=0):
+    days = randint(0,days)
+    ref_datetime = datetime.utcnow()-timedelta(days=days)
+    event_time = ref_datetime.isoformat()
     event_time = event_time.split(':')
     event_time[-1] = "%.3f" %(float(event_time[-1]))
     event_time = ':'.join(event_time)+'Z'
@@ -21,10 +26,10 @@ def fake_key(n=12):
 def fake_amount(max_dollars=100.0):
     amount = "%.2f" %(random.uniform(0.1,max_dollars))
     amount = amount +' USD'
-    return fake_amount
+    return amount
 
 
-def fake_customer(verb='NEW'):
+def fake_customer(verb='NEW',key=fake_key(),days=0):
     event_type = 'CUSTOMER'
     event_verb = verb #'NEW' or 'UPDATE'
     customer = {}
@@ -34,26 +39,26 @@ def fake_customer(verb='NEW'):
     customer['last_name'] = faker.last_name()
     customer['adr_state'] = faker.state_abbr()
     customer['adr_city'] = faker.city()
-    event_time = fake_event_time()
+    event_time = fake_event_time(days=days)
     customer['event_time'] = event_time
-    customer['key'] = fake_key()
+    customer['key'] = key
     return json.dumps(customer)
 
-def fake_site_vist():
+def fake_site_vist(customer_key=fake_key(),days=0):
     event_type = 'SITE_VISIT'
     event_verb = 'NEW'
     site_visit = {}
     site_visit['type'] = event_type
     site_visit['verb'] = event_verb
     faker = Factory.create()
-    event_time = fake_event_time()
+    event_time = fake_event_time(days=days)
     site_visit['event_time'] = event_time
     site_visit['key'] = fake_key()
-    site_visit['customer_id'] = fake_key()
+    site_visit['customer_id'] = customer_key
     site_visit['tags'] = {'some key': 'some value'}
     return json.dumps(site_visit)
 
-def fake_image_upload():
+def fake_image_upload(customer_key=fake_key(),days=0):
     event_type = 'IMAGE'
     event_verb = 'UPLOAD'
     image_upload = {}
@@ -62,15 +67,15 @@ def fake_image_upload():
     faker = Factory.create()
     faker.add_provider(CameraProvider)
     camera_make,camera_model = faker.camera()
-    event_time = fake_event_time()
+    event_time = fake_event_time(days=days)
     image_upload['event_time'] = event_time
     image_upload['key'] = fake_key()
-    image_upload['customer_id'] = fake_key()
+    image_upload['customer_id'] = customer_key
     image_upload['camera_make'] = camera_make
     image_upload['camera_model'] = camera_model
-    return json.dumps(site_visit)
+    return json.dumps(image_upload)
 
-def fake_order(verb='NEW'):
+def fake_order(verb='NEW',customer_key=fake_key(),days=0):
     event_type = 'ORDER'
     event_verb = verb #'NEW' or 'UPDATE'
     customer_order = {}
@@ -78,7 +83,7 @@ def fake_order(verb='NEW'):
     customer_order['verb'] = event_verb
     customer_order['total_amount'] = fake_amount()
     customer_order['key'] = fake_key()
-    event_time = fake_event_time()
+    event_time = fake_event_time(days=days)
     customer_order['event_time'] = event_time
-    customer_order['customer_id'] = fake_key()
+    customer_order['customer_id'] = customer_key
     return json.dumps(customer_order)
---
+++ src/data/run_fake_events.py
@@ -0,0 +1,23 @@
+import fake_events
+from random import randint
+
+DAYS=14
+N_CUSTOMERS=50
+MAX_EVENTS=5
+
+EVENTS = []
+for i in range(N_CUSTOMERS):
+    customer_key = fake_events.fake_key()
+    for j in range(randint(1,MAX_EVENTS)):
+        customer = fake_events.fake_customer(key=customer_key,days=DAYS)
+        site_visit = fake_events.fake_site_vist(customer_key=customer_key,days=DAYS)
+        image_upload = fake_events.fake_image_upload(customer_key=customer_key,days=DAYS)
+        customer_order = fake_events.fake_order(customer_key=customer_key,days=DAYS)
+        EVENTS.append(customer)
+        EVENTS.append(site_visit)
+        EVENTS.append(image_upload)
+        EVENTS.append(customer_order)
+
+lines = '\n'.join(EVENTS)
+with open('../../input/input.txt','w') as f:
+    f.write(lines)
