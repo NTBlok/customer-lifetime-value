2caf3d2a140457a27d7825773694636862fbb6d9
NTBlok <ntedeschiblok@gmail.com>
2017-02-24 09:51:32 -0800

    Fixed bug in sessionization

--- src/data/run_fake_events.py
+++ src/data/run_fake_events.py
@@ -8,8 +8,8 @@ MAX_EVENTS=5
 EVENTS = []
 for i in range(N_CUSTOMERS):
     customer_key = fake_events.fake_key()
+    customer = fake_events.fake_customer(key=customer_key,days=DAYS)
     for j in range(randint(1,MAX_EVENTS)):
-        customer = fake_events.fake_customer(key=customer_key,days=DAYS)
         site_visit = fake_events.fake_site_vist(customer_key=customer_key,days=DAYS)
         image_upload = fake_events.fake_image_upload(customer_key=customer_key,days=DAYS)
         customer_order = fake_events.fake_order(customer_key=customer_key,days=DAYS)
