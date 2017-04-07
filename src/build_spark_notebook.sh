9f1f142b0f442c03ac4b332a22df32452c169a01
NTBlok <ntedeschiblok@gmail.com>
2017-04-07 10:07:43 -0700

    Restructure for testing

---
+++ src/build_spark_notebook.sh
@@ -0,0 +1,1 @@
+docker build --rm -t jupyter/pyspark-notebook-pytest .
--- src/start_spark_notebook.sh
+++ src/start_spark_notebook.sh
@@ -1,1 +1,2 @@
-docker run -it --rm -p 8888:8888 -v $(dirname $(pwd)):/home/jovyan/work   --name pyspark-etl jupyter/pyspark-notebook
+#docker run -it --rm -p 8888:8888 -v $(dirname $(pwd)):/home/jovyan/work   --name pyspark-etl jupyter/pyspark-notebook
+docker run -it --rm -p 8888:8888 -v $(dirname $(pwd)):/home/jovyan/work   --name pyspark-etl jupyter/pyspark-notebook-pytest
